111976f8d0351296107ec337b876248af60a2911
pquantum <pr.pascal.bachmann@gmail.com>
2019-07-05 21:58:51 +0200

    add color functions

--- src/Utils/color.js
+++ src/Utils/color.js
@@ -44,3 +44,68 @@ export function getContrastColor(hex) {
 		return "#ffffff";
 	}
 }
+
+
+/**
+ *
+ * @param {string} hex - hex color
+ * @param {number} amt - ammount 100 = lighten / -100 = darken
+ */
+export function lightenDarkenColor(hex, percent) {
+
+    var num = parseInt(hex,16),
+        amt = Math.round(2.55 * percent),
+        R = (num >> 16) + amt,
+        B = (num >> 8 & 0x00FF) + amt,
+        G = (num & 0x0000FF) + amt;
+
+        return (0x1000000 + (R<255?R<1?0:R:255)*0x10000 + (B<255?B<1?0:B:255)*0x100 + (G<255?G<1?0:G:255)).toString(16).slice(1);
+
+}
+
+
+
+/**
+ *
+ * @param {string} color - HEX
+ * @param {number} amount - percentage
+ */
+export const lighten = (color, amount)=> {
+    color = (color.indexOf("#")>=0) ? color.substring(1,color.length) : color;
+    amount = parseInt((255*amount)/100);
+    return color = `#${addLight(color.substring(0,2), amount)}${addLight(color.substring(2,4), amount)}${addLight(color.substring(4,6), amount)}`;
+}
+/**
+ *
+ * @param {string} color - HEX
+ * @param {number} amount - percentage
+ */
+export const darken = (color, amount) =>{
+    color = (color.indexOf("#")>=0) ? color.substring(1,color.length) : color;
+    amount = parseInt((255*amount)/100);
+    return color = `#${subtractLight(color.substring(0,2), amount)}${subtractLight(color.substring(2,4), amount)}${subtractLight(color.substring(4,6), amount)}`;
+}
+
+
+/**
+ *
+ * @param {string} color - RGB
+ * @param {number} amount - percentage
+ */
+const subtractLight = function(color, amount){
+    let cc = parseInt(color,16) - amount;
+    let c = (cc < 0) ? 0 : (cc);
+    c = (c.toString(16).length > 1 ) ? c.toString(16) : `0${c.toString(16)}`;
+    return c;
+}
+/**
+ *
+ * @param {string} color - RGB
+ * @param {number} amount - percentage
+ */
+const addLight = function(color, amount){
+    let cc = parseInt(color,16) + amount;
+    let c = (cc > 255) ? 255 : (cc);
+    c = (c.toString(16).length > 1 ) ? c.toString(16) : `0${c.toString(16)}`;
+    return c;
+}
