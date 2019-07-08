56868a63ca0a3373ebe0086c5f7444d6704db3d6
pquantum <pr.pascal.bachmann@gmail.com>
2019-07-08 18:27:17 +0200

    add headbar

--- src/App.svelte
+++ src/App.svelte
@@ -3,15 +3,54 @@
   import UiComponents from "./demo/UiComponents.svelte";
   import { Screen } from "./components/Layout";
 
-  export let name;
+  import { Ripple } from "./components/Ripple";
+  import { ToggleButton } from "./components/Toggle";
+  import { ToggleGroup } from "./components/Toggle";
 </script>
 
 <style>
-  h1 {
-    color: purple;
+  :global(body) {
+    margin: 0;
+  }
+  .headbar {
+    width: 100%;
+    height: 55px;
+    display: flex;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.25);
+    margin-bottom: 20px;
+    color: rgba(0, 0, 0, 0.85);
+    align-items: center;
+  }
+  .headbar-center {
+    max-width: 1200px;
+    margin: 0 auto;
+    width: 100%;
+    display: flex;
+    align-items: center;
+  }
+  .headbar-link {
+    display: inline-flex;
+    border-radius: 5px;
+    padding: 8px 10px;
+    cursor: pointer;
+    position: relative;
+    font-size: 14px;
   }
 </style>
 
+<div class="headbar">
+  <div class="headbar-center">
+    <div class="headbar-link">
+      Textfields
+      <Ripple color="#000000" />
+    </div>
+
+    <ToggleGroup useNestedElements={false}>
+      <ToggleButton>Circle Navigation</ToggleButton>
+    </ToggleGroup>
+
+  </div>
+</div>
 <Screen>
   <UiComponents />
 </Screen>
